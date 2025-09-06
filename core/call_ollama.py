5e86b4c9683d170ba9caedb3ac845ec04d2e1674
NgJaBach <ngbach2008@email.com>
2025-09-07 02:05:54 +0700

    update

---
+++ core/call_ollama.py
@@ -0,0 +1,41 @@
+from openai import OpenAI
+import openai
+import backoff
+import re
+
+client = OpenAI(base_url='http://localhost:11434/v1/', api_key='ollama')
+
+@backoff.on_exception(
+    backoff.expo,
+    (openai.RateLimitError, openai.BadRequestError, openai.InternalServerError),
+    max_time=999, max_tries=9999
+)
+def completions_with_backoff(**kwargs):
+    return client.chat.completions.create(**kwargs)
+
+def remove_reasoning(response_content: str) -> str:
+    match = re.search(r"</think>\s*(.*)", response_content, re.DOTALL)
+    if match:
+        final_answer = match.group(1).strip()
+        return final_answer
+    else:
+        return response_content.strip()
+
+def ask(user: str, 
+        llm_name="gpt-oss:20b", 
+        max_token=128000,
+        temperature=0.3,
+        reasoning_effort="low"
+        ) -> str:
+    response = completions_with_backoff(
+        model=llm_name,
+        messages=[
+            {"role": "user", "content": user},
+        ],
+        max_tokens=max_token,
+        n=1,
+        stop=None,
+        temperature=temperature,
+        extra_body={"reasoning_effort": reasoning_effort}
+    ).choices[0].message.content
+    return remove_reasoning(response)
