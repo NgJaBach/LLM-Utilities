6fc79000efda26a912e959889ab516fc1ad2739f
NgJaBach <ngbach2008@email.com>
2025-09-16 00:14:42 +0700

    update

--- call-llm-simple/openai_chat.py
+++ call-llm-simple/openai_chat.py
@@ -24,20 +24,37 @@ def remove_reasoning(response_content: str) -> str:
     else:
         return response_content.strip()
 
-def ask(user: str, 
+def ask(user_prompt: str, 
+        sys_prompt: str = "",
         llm_name="gpt-oss:20b", 
         max_token=128000,
         temperature=0.3,
+        reasoning_level=None, # ["low", "medium", "high"]
         ) -> str:
-    response = completions_with_backoff(
-        model=llm_name,
-        messages=[
-            {"role": "user", "content": user},
-        ],
-        max_tokens=max_token,
-        n=1,
-        stop=None,
-        temperature=temperature,
-        reasoning={"effort": "medium"},
-    ).choices[0].message.content
+    if reasoning_level == None:
+        response = completions_with_backoff(
+            model=llm_name,
+            messages=[
+                {"role": "system", "content": sys_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            max_tokens=max_token,
+            n=1,
+            stop=None,
+            temperature=temperature,
+        ).choices[0].message.content
+    else:
+        response = completions_with_backoff(
+            model=llm_name,
+            messages=[
+                {"role": "system", "content": sys_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            max_tokens=max_token,
+            n=1,
+            stop=None,
+            temperature=temperature,
+            reasoning={"effort": reasoning_level},
+            extra_body={"reasoning_effort": reasoning_level}
+        ).choices[0].message.content
     return remove_reasoning(response)
